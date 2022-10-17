ddfccf95e6221a0f2caed7f6917c15039cc5e3b5
Joao Esperancinha <jofisaes@gmail.com>
2022-10-17 12:24:52 +0200

    First connection to PostgreSQL native!

--- whiskers-red-cat-db/Readme.md
+++ whiskers-red-cat-db/Readme.md
@@ -2,4 +2,9 @@
 
 ```shell
 sudo apt install libreadline-dev
+sudo apt-get install libpq-dev
+sudo apt install bison
+sudo apt install flex
 ```
+
+- [postgres-native-sqldelight](https://github.com/hfhbd/postgres-native-sqldelight)
--- whiskers-red-cat-db/main.kt
+++ whiskers-red-cat-db/main.kt
@@ -6,12 +6,15 @@ import redcat.*
 fun main(args: Array<String>) {
     println("OOOOOOO")
     val conn = PQsetdbLogin(
-        pghost = "host",
-        pgport = "",
+        pghost = "localhost",
+        pgport = "5432",
         pgtty = null,
-        dbName = "database",
-        login = "user",
-        pwd = "",
+        dbName = "whiskers",
+        login = "whiskers",
+        pwd = "whiskers",
         pgoptions = "options"
     )
+    println(PQstatus(conn))
+    println("AAAAAA")
+//    println(executeCommand("PGPASSWORD=red_cat psql -U whiskers -d whiskers -c 'SELECT 1' -h localhost"))
 }
