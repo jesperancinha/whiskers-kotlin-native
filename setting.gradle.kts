622be80b6d842ce2a125bddaf46413f7fcb4a2a6
Joao Esperancinha <jofisaes@gmail.com>
2022-10-26 00:45:11 +0200

    Gradle stuff

---
+++ setting.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = 'whiskers-kotlin-native-command-line'
+include ':whiskers-graalvm'
+include ':whiskers-ktor'
+include ':whiskers-ktor-harcoded'
+include ':whiskers-cloudnative'
+include ':good-feel'
+include ':plus'
+
+
