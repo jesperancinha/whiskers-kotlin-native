3dc0edb52abb5f4531791f23b2c892c930ba53e7
Joao Esperancinha <jofisaes@gmail.com>
2022-10-29 10:16:34 +0200

    Live test readings

---
+++ Results.md
@@ -0,0 +1,8 @@
+# Whiskers Performance results
+
+| Architecture | Sartup time (seconds) | Memory used(Mbytes) | Test with DB connection(s) | Test Mixed DB + Algorithm(s) | Test Algorithm (s) |
+|---|---|---|---|---|---|
+|cloudnative|6|  44.79MiB / 15.43GiB|27|0|2|
+|graalvm|13|  45.17MiB / 15.43GiB|28|40|16|
+|jvm|13|  156.2MiB / 15.43GiB|29|18|21|
+|ktor|11|  2.238MiB / 15.43GiB|9|1|79|
--- whiskers-paragraph-sender/make_stats.py
+++ whiskers-paragraph-sender/make_stats.py
@@ -1,5 +1,13 @@
 cases = ['cloudnative', 'graalvm', 'jvm', 'ktor']
-
+title = "# Whiskers Performance results\n"
+header = '| Architecture | Sartup time (seconds) | Memory used(Mbytes) | Test with DB connection(s) | Test Mixed DB + ' \
+         'Algorithm(s) | Test Algorithm (s) |\n'
+separator = '|---|---|---|---|---|---|\n'
+f = open("../Results.md", "w")
+f.write(title)
+f.write("\n")
+f.write(header)
+f.write(separator)
 for case in cases:
     with open("../result-{0}.csv".format(case)) as startupTS:
         startups = startupTS.read().splitlines()
@@ -10,3 +18,10 @@ for case in cases:
     with open("../result-mem-{0}.txt".format(case)) as usedMems:
         mems = usedMems.read().splitlines()
         print(mems)
+    startupTime = int(startups[1].split(',')[1]) - int(startups[0].split(',')[1])
+    memUsage = mems[1].split('   ')[3]
+    timeDbConn = int(calls[1].split(',')[1]) - int(calls[0].split(',')[1])
+    timeMix = int(calls[3].split(',')[1]) - int(calls[2].split(',')[1])
+    timeAlgorithm = int(calls[5].split(',')[1]) - int(calls[4].split(',')[1])
+    f.write("|{0}|{1}|{2}|{3}|{4}|{5}|\n".format(case, startupTime, memUsage, timeDbConn, timeMix, timeAlgorithm))
+f.close()
