5270309b2b47ad05b2e73a1cbd3baeb55596a5f6
Joao Esperancinha <jofisaes@gmail.com>
2022-10-30 10:28:28 +0100

    Makes tests consistent

--- whiskers-paragraph-sender/test_all.py
+++ whiskers-paragraph-sender/test_all.py
@@ -2,7 +2,7 @@ from paragraph_sender import send_paragraphs_no_log
 import requests
 
 
-for num in range(1, 301):
+for num in range(1, 501):
     send_paragraphs_no_log()
 
     requests.get('http://localhost:8080/story/paragraphs')
