03758e198a0c7afe9d5c100c361d59573b2ba960
Joao Esperancinha <jofisaes@gmail.com>
2022-11-02 21:53:03 +0100

    Adds some tests

--- plus/src/nativeMain/kotlin/Main.kt
+++ plus/src/nativeMain/kotlin/Main.kt
@@ -1,22 +1,30 @@
-import io.ktor.util.*
-
 fun main() {
     println("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*")
     println(randomMessage())
     println("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*")
-    HashMap<String,String>()
+    val groupBy: Map<String, List<Int>> = allMessages().groupBy({ it }, { it.length })
+    val hashMap = allMessages().groupBy({ it }, { it.length }).entries.fold(HashMap<String, List<Int>>()) { a, b ->
+            a[b.key] = b.value
+            a
+        }
+    println(groupBy::class.qualifiedName)
+    println(hashMap::class.qualifiedName)
+    val toTypedArray: Map<String, String> = allMessages().toTypedArray().associateBy { it }
+    println(toTypedArray::class.qualifiedName)
 }
 
 fun randomMessage(): String {
-   return listOf(
-       "Good Morning!",
-       "You are looking great today!",
-       "What a great day today!",
-       "Good job!",
-       "I really appreciate what you just did! Thank you!",
-       "Thanks for bringing me coffee!",
-       "You are the best pal ever!",
-       "I love working with you!",
-       "Rise and shine!"
-   ).random()
+    return allMessages().random()
 }
+
+private fun allMessages() = listOf(
+    "Good Morning!",
+    "You are looking great today!",
+    "What a great day today!",
+    "Good job!",
+    "I really appreciate what you just did! Thank you!",
+    "Thanks for bringing me coffee!",
+    "You are the best pal ever!",
+    "I love working with you!",
+    "Rise and shine!"
+)
