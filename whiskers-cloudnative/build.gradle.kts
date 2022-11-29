74880e0bc9e33e4b4c3010caa036981458d07989
Joao Esperancinha <jofisaes@gmail.com>
2022-11-29 10:58:44 +0100

    id("org.graalvm.buildtools.native' version") version "0.9.18"

--- whiskers-cloudnative/build.gradle.kts
+++ whiskers-cloudnative/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("org.springframework.boot") version "3.0.0"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("org.springframework.experimental.aot") version "0.12.1"
-	id("org.graalvm.buildtools.native' version") version "0.9.18"
+	id("org.graalvm.buildtools.native") version "0.9.18"
 	kotlin("jvm") version "1.7.21"
 	kotlin("plugin.spring") version "1.7.21"
 }
