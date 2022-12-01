3316670732a728d6ef081865ea80367f4c75df6e
Joao Esperancinha <jofisaes@gmail.com>
2022-12-01 22:06:40 +0100

    GraalVM upgrade

--- whiskers-runners/graalVM.sh
+++ whiskers-runners/graalVM.sh
@@ -2,5 +2,5 @@
 
 echo -e '\033[92mChanging to the first available SDK MAN GraalVM version.\033[0m'
 echo -e '\033[91mRemember to run this command like this: . ./graalVM.sh\033[0m'
-sdk install java 22.2.r17-grl
+sdk install java 22.3.r17-grl
 sdk use java $(sdk list java | grep installed | grep "\-grl" |  cut -d'|' -f6- | cut -d' ' -f2- )
--- whiskers-runners/whiskers-runners-graalvm/graalVM.sh
+++ whiskers-runners/whiskers-runners-graalvm/graalVM.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-sdk install java 22.2.r17-grl
-sdk use java 22.2.r17-grl
+sdk install java 22.3.r17-grl
+sdk use java 22.3.r17-grl
